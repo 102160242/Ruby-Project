f4f68cbcf3954ad1efcc5a74b9f0d9c76262d992
Baochanh18 <baochanh18@gmail.com>
2019-04-04 21:31:19 +0700

    - Tạo validates với user và category

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,14 @@ class User < ApplicationRecord
     has_many :tests, dependent: :destroy
 
     before_destroy { words.clear }
+
+    before_save { self.email = email.downcase }
+    validates :username, presence: true, length: { maximum: 15, minimum: 6 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+    has_secure_password
+    validates :password_digest, presence: true, length: { minimum: 6 }
+                  
 end
