ce3600417354148210b1ffbc0346d29f2cbc5ace
Baochanh18 <baochanh18@gmail.com>
2019-04-12 20:23:16 +0700

    admin test done, admin question chua hoan thanh

---
+++ app/controllers/admin/answers_controller.rb
@@ -0,0 +1,81 @@
+class Admin::AnswersController < ApplicationController
+    def index
+        @answers = Answer.all
+      end
+    
+      # GET /answers/1
+      # GET /answers/1.json
+      def show
+        @answer = Answer.find(params[:id])
+      end
+    
+      # GET /answers/new
+      def new
+        @answer = Answer.new
+      end
+      
+      def edit
+        @answer = Answer.find(params[:id])
+      end
+      # GET /answers/1/edit
+    #   def edit
+    #     @answer = Answer.where("answer.id = #{params[:id]}")
+    #     respond_to do |format|
+    #         format.html render { redirect_to root_url, notice: params[:id] }
+    #     end
+    #   end
+    
+      # POST /answers
+      # POST /answers.json
+      def create
+        @answer = Answer.new(answer_params)
+    
+        respond_to do |format|
+          if @answer.save
+            format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
+            format.json { render :show, status: :created, location: @answer }
+          else
+            format.html { render :new }
+            format.json { render json: @answer.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+    
+      # PATCH/PUT /answers/1
+      # PATCH/PUT /answers/1.json
+      def update
+        @answer = Answer.find(params[:id])
+        respond_to do |format|
+          if @answer.update(answer_params)
+            format.html { redirect_to admin_answer_path, notice: 'Answer was successfully updated.' }
+            format.json { render :show, status: :ok, location: @answer }
+          else
+            format.html { render :edit }
+            format.json { render json: @answer.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+    
+      # DELETE /answers/1
+      # DELETE /answers/1.json
+      def destroy
+        @answer = Answer.find(params[:id])
+        @answer.destroy
+        respond_to do |format|
+          format.html { redirect_to admin_answers_path, notice: 'Answer was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+      end
+    
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_answer
+          @answer = Answer.find(params[:id])
+        end
+    
+        # Never trust parameters from the scary internet, only allow the white list through.
+        def answer_params
+          params.require(:answer).permit(:question_id, :answer_content, :right_answer)
+        end
+    end
+    
